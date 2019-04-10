1846e77f6ad8d407ade3dff227e793e5393887d7
Ryan Herr <ryanherr@Ryans-MBP.zyxel.com>
2019-04-09 19:35:50 -0500

    Add templates, routes, API authentication

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,32 @@
 """Main application and routing logic for TwitOff."""
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template
+from .models import DB, User
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = getenv('FLASK_ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to TwitOff!'
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
+    @app.route('/user/<name>')
+    def user(name):
+        tweets = User.query.filter(User.name == name).first().tweets
+        return render_template('user.html', title=name, tweets=tweets)
 
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -7,6 +7,8 @@ class User(DB.Model):
     """Twitter users that we pull and analyze tweets for."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
@@ -14,7 +16,8 @@ class User(DB.Model):
 class Tweet(DB.Model):
     """Tweets."""
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(300))  # Allowing for full + links
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
